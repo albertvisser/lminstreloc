aa0d2abefed6f53e73113660d2f3b5a048bc8ffd
Albert Visser <albert.visser@gmail.com>
2022-12-16 23:02:31 +0100

    corrected an oopsie

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 """starter program for lminstreloc: a tool to correct filenames for instruments in LMMS modules
 """
-mport sys
+import sys
 from rewrite_lmmsfile import copyfile
 
 copyfile(sys.argv[1])
