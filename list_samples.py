24c9a72a1e6bca5fb1abccaa004509e320180c6b
Albert Visser <albert.visser@gmail.com>
2022-12-28 12:04:02 +0100

    output naar stdout ook mogelijk

--- list_samples.py
+++ list_samples.py
@@ -3,6 +3,7 @@
 import sys
 import samplelister
 
-with open(sys.argv[2], 'w') as f:
+out = open(sys.argv[2], 'w') if len(sys.argv) > 2 else sys.stdout
+with out:
     for line in samplelister.list_samples(sys.argv[1]):
         print(line, file=f)
